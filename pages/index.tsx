fc72ac58a28641c2ddb3b79e668163016fb31439
Kirill Taratun <taratunse@gmail.com>
2025-12-03 15:03:57 +0700

    добавление папки pages с компонентами страниц и API

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import {getSession, signOut} from "next-auth/react";
+import {NextPageContext} from "next";
+import NavBar from "@/components/NavBar";
+
+// Функция для редиректа не авторизованных пользователей на /auth
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context)
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false
+            }
+        }
+    }
+    return {
+        props: {}
+    }
+}
+
+export default function Home() {
+  return (
+    <>
+        <NavBar />
+    </>
+  );
+}
