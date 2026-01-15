303272fb82cd5d8a9c8c793e821b714bd35b3806
Kirill Taratun <taratunse@gmail.com>
2026-01-15 16:33:44 +0700

    добавление имени аккаунта

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,11 +1,14 @@
 import { signOut } from "next-auth/react";
 import {FC} from "react";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface AccountMenuProps {
     visible?: boolean
 }
 
 const AccountMenu: FC<AccountMenuProps> = ({ visible }) => {
+    const { data } = useCurrentUser();
+
     if (!visible) {
         return null
     }
@@ -25,7 +28,7 @@ const AccountMenu: FC<AccountMenuProps> = ({ visible }) => {
             <div className="flex flex-col gap-3">
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img className="w-8 rounded-md" src="/images/profiles/default-blue.png" alt="profile-logo"/>
-                    <p className="text-white text-sm group-hover/item:underline">Username</p>
+                    <p className="text-white text-sm group-hover/item:underline">{ data?.name }</p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4"/>
                 <div onClick={() => signOut()} className="px-3 text-center text-white text-sm hover:underline">
